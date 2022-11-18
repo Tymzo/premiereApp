9764667cd409907d04cf739132ae66cd82b08a51
Tymzo <stefan.antnj@eduge.ch>
2022-11-18 09:48:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resutText: TextView = findViewById(R.id.result_Text)
+
+        val randomInt = Random().nextInt(6) + 1
+
+
+        resutText.text = randomInt.toString()
+    }
+
+}
